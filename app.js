55d3a3cc16bd2724b2c50155591b1b79e21d7851
Marquis <78649236+ToxicMarquis@users.noreply.github.com>
2025-07-01 18:58:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Константы системы уровней и конфигурации
 const BASE_EXPERIENCE = 50;
-const LEVEL_MULTIPLIER = 1.5;
+const LEVEL_MULTIPLIER = 1.1;
 
 // Переменные для работы с данными
 let allPlayerData = [];
